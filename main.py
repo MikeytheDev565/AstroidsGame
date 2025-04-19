e2f82bf4ed3752e1a807e881ab8172f589a171aa
MikeytheDev565 <Mikeytheguy565@gmail.com>
2025-04-18 21:36:12 -0600

    now has asteroids but no collsions

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
 from constants import *
 from player import *
-
+from asteroidfield import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+   
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidField = AsteroidField()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,8 +25,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        
+        for x in drawable:
+            x.draw(screen)
+        
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
